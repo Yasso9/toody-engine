97ffe37710551760e5e932f4b74084f9efe2458a
Ilyas Turki <turki.ilyass@email.com>
2022-04-07 11:56:20 +0200

    Improvement of the python script to compile libraries. Can now read path from command line

--- tools/compile_libraries.py
+++ tools/compile_libraries.py
@@ -1,12 +1,35 @@
 import os
 import pathlib
-
-# Project Directory is in the parent folder of this file
-projectDirectoryPath = str(pathlib.Path().absolute().parents[0])
-librariesObjectPath = projectDirectoryPath + "/external/object"
+import sys
+from dataclasses import dataclass
 
 
-def compile_glad(gladDirectory):
+@dataclass
+class Paths:
+    objectCreated: str
+    gladLibrary: str
+    sqliteLibrary: str
+
+
+def launch_error(errorMessage: str):
+    print("ERROR :" + errorMessage, file=sys.stderr)
+    sys.exit(1)
+
+
+def extract_arguments():
+    argumentList = sys.argv[1:]
+
+    if len(argumentList) != 3:
+        launch_error("This script should have 3 args.")
+
+    for argument in argumentList:
+        if not os.path.isdir(argument):
+            launch_error("All the args should be directories that exist")
+
+    return Paths(argumentList[0], argumentList[1], argumentList[2])
+
+
+def compile_glad(objectCreatedDirectory, gladDirectory):
     # Filename we need to generate
     gladObjectFileName = "glad.o"
 
@@ -16,15 +39,15 @@ def compile_glad(gladDirectory):
     )
 
     # Delete object file if he already exist
-    oldGladObjectPath = librariesObjectPath + "/" + gladObjectFileName
+    oldGladObjectPath = objectCreatedDirectory + "/" + gladObjectFileName
     if os.path.isfile(oldGladObjectPath):
         os.remove(oldGladObjectPath)
 
     # # Copy object file to oppropriate directory
-    os.system(f"mv {gladObjectFileName} {librariesObjectPath}")
+    os.system(f"mv {gladObjectFileName} {objectCreatedDirectory}")
 
 
-def compile_sqlite(sqliteDirectory):
+def compile_sqlite(objectCreatedDirectory, sqliteDirectory):
     # Filename we need to generate
     sqliteObjectFileName = "sqlite.o"
 
@@ -32,31 +55,16 @@ def compile_sqlite(sqliteDirectory):
     os.system(f"clang -c {sqliteDirectory}/sqlite3.c -o {sqliteObjectFileName}")
 
     # Delete object file if he already exist
-    oldSqliteObjectPath = librariesObjectPath + "/" + sqliteObjectFileName
+    oldSqliteObjectPath = objectCreatedDirectory + "/" + sqliteObjectFileName
     if os.path.isfile(oldSqliteObjectPath):
         os.remove(oldSqliteObjectPath)
 
     # # Copy object file to oppropriate directory
-    os.system(f"mv {sqliteObjectFileName} {librariesObjectPath}")
+    os.system(f"mv {sqliteObjectFileName} {objectCreatedDirectory}")
 
 
-# gladDirectory = "/home/ineo/Developpement/CPP_LIBRAIRIES/GLAD"
-# sqliteDirectory = "/home/ineo/Developpement/CPP_LIBRAIRIES/SQLITE"
+if __name__ == "__main__":
+    paths = extract_arguments()
 
-gladDirectory = "C:/Users/Turki/GoogleDrive/Programs/Programming/CPP_LIBRAIRIES/GLAD"
-sqliteDirectory = "C:/Users/Turki/GoogleDrive/Programs/Programming/CPP_LIBRAIRIES/SQLITE"
-
-compile_glad(gladDirectory)
-compile_sqlite(sqliteDirectory)
-
-
-# gladPath = "C:/Developpement/CPP_LIBRAIRIES/GLAD"
-# sqlitePath = "C:/Developpement/CPP_LIBRAIRIES/SQLITE"
-
-
-# cd "C:/Developpement/CPP_LIBRAIRIES/GLAD"
-# clang -c glad/glad.c -o glad.o -I"./"
-# cp glad.o "C:/Users/Turki/GoogleDrive/Computing/Projects/ToodyEngine/object_libraries"
-# cd "C:/Developpement/CPP_LIBRAIRIES/SQLITE/sources/SQLITE"
-# clang -c sqlite3.c -o sqlite3.o
-# cp sqlite3.o "C:/Users/Turki/GoogleDrive/Computing/Projects/ToodyEngine/object_libraries"
+    compile_glad(paths.objectCreated, paths.gladLibrary)
+    compile_sqlite(paths.objectCreated, paths.sqliteLibrary)
